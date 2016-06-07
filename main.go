cc8ddf8e49b79885863dbdfef4b3f6eed3d8da90
busizshen <78732833@qq.com>
2016-06-07 15:06:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"/api/v1/root"
-	"/api/v1/session"
-	"/web"
+	"api/v1/root"
+	"api/v1/session"
+	"web"
 
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
